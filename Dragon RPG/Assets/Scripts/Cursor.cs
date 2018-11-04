ae6a17875b50d165d3ed59d7c97d3b63b59aa04b
EyiLee <eyilee@protonmail.com>
2018-11-04 16:23:02 +0800

    [add] set the environment, player and camera

---
+++ Dragon RPG/Assets/Scripts/Cursor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+    CameraRaycaster cameraRaycaster;
+
+    private void Start () {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+    }
+
+    private void Update () {
+        print(cameraRaycaster.layerHit);
+    }
+}
