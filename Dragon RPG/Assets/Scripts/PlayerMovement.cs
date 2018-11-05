8a2abe0ff33c6e0eb243f6a444f3818ac97813bd
EyiLee <eyilee@protonmail.com>
2018-11-05 18:07:16 +0800

    [add] click to move player

---
+++ Dragon RPG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] float walkMoveStopRadius = 0.2f;
+
+    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    CameraRaycaster cameraRaycaster;
+    Vector3 currentClickTarget;
+
+    private void Start () {
+        cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+        m_Character = GetComponent<ThirdPersonCharacter>();
+        currentClickTarget = transform.position;
+    }
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate () {
+        if (Input.GetMouseButton(0)) {
+            switch (cameraRaycaster.layerHit) {
+                case Layer.Walkable:
+                    currentClickTarget = cameraRaycaster.hit.point;
+                    break;
+                case Layer.Enemy:
+                    break;
+                case Layer.RaycastEndStop:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        Vector3 playerToClickPoint = currentClickTarget - transform.position;
+        if (playerToClickPoint.magnitude >= walkMoveStopRadius) {
+            m_Character.Move(playerToClickPoint, false, false);
+        }
+    }
+}
+
