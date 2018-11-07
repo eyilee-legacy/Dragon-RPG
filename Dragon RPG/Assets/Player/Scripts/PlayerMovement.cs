1160c938d948a2f615973c316c3fc1cc85072175
EyiLee <eyilee@protonmail.com>
2018-11-07 15:11:51 +0800

    [modify] toggle move mode by KeyCode.G, remove ThirdPersonUserControl from player

--- Dragon RPG/Assets/Player/Scripts/PlayerMovement.cs
+++ Dragon RPG/Assets/Player/Scripts/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour {
 
     [SerializeField] float walkMoveStopRadius = 0.2f;
 
-    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
-    CameraRaycaster cameraRaycaster;
-    Vector3 currentClickTarget;
+    private ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    private CameraRaycaster cameraRaycaster;
+    private Vector3 currentClickTarget;
+
+    private bool isInDirectMode = false;
 
     private void Start () {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
@@ -19,6 +21,28 @@ public class PlayerMovement : MonoBehaviour {
 
     // Fixed update is called in sync with physics
     private void FixedUpdate () {
+        if (Input.GetKey(KeyCode.G)) {
+            isInDirectMode = !isInDirectMode;
+        }
+
+        if (isInDirectMode) {
+            ProcessDirectMovement();
+        } else {
+            ProcessMouseMovement();
+        }
+    }
+
+    private void ProcessDirectMovement () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+        m_Character.Move(m_Move, false, false);
+    }
+
+    private void ProcessMouseMovement () {
         if (Input.GetMouseButton(0)) {
             switch (cameraRaycaster.layerHit) {
                 case Layer.Walkable:
@@ -36,6 +60,8 @@ public class PlayerMovement : MonoBehaviour {
         Vector3 playerToClickPoint = currentClickTarget - transform.position;
         if (playerToClickPoint.magnitude >= walkMoveStopRadius) {
             m_Character.Move(playerToClickPoint, false, false);
+        } else {
+            m_Character.Move(Vector3.zero, false, false);
         }
     }
 }
